976e592f36c27adac4483bdad51a32d7b187e6d9
Candi Mahendra <abdumaadjidcandi@gmail.com>
2025-08-11 14:46:46 +0700

    Init Spring Boot project

---
+++ src/main/java/com/mahendracandi/searchtechnique/SearchTechniqueApplication.java
@@ -0,0 +1,13 @@
+package com.mahendracandi.searchtechnique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchTechniqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchTechniqueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mahendracandi/searchtechnique/SearchTechniqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahendracandi.searchtechnique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchTechniqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
